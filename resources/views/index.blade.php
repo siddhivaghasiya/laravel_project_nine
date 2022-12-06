0a7181859d8dc2d0c4925ebcfa9ccae00f549999
chiragghevariya <siddhivaghasiya3@gmail.com>
2022-12-06 14:58:14 +0530

    relationship

---
+++ resources/views/index.blade.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <style>
+        form {
+            margin-top: 27px;
+            margin-right: 250px;
+        }
+
+        .card.card-primary {
+            margin-top: 40px;
+        }
+
+        .container {
+            width: 1046px;
+        }
+
+        .error {
+            color: red;
+        }
+    </style>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <script src="{{ asset('theme/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('theme/additional-methods.min.js') }}"></script>
+</head>
+
+<body>
+
+
+
+    <h3>
+        <a href="">Members</a>
+    </h3>
+    <h3>
+        <a href="">Companies</a>
+    </h3>
+
+</body>
+
+</html>
